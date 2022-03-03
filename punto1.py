64cf0b6ce98fef2c3bc53171b264e83caa1f989b
juan ariza <jariza36@cuc.edu.co>
2022-03-02 21:08:52 -0500

    segundo punto

--- punto1.py
+++ punto1.py
@@ -5,14 +5,14 @@ Created on Wed Mar  2 20:50:32 2022
 @author: jariza36
 """
 
-donaciondevalor = int(input("valor total"))
+valordonado = int(input("valor total"))
 valortele = valordonado*0.1
 valosiste = valordonado*0.25
 valoradmi = valordonado*0.35
-vcontabilidad = valortele - valosiste - valoadmi
+vcontabilidad = valortele - valosiste - valoradmi
 
-print(f'El valor del donado es de: ${valortel:,}')
-print(f'El valor del  donad  es de: ${valosistee:,}')
+print(f'El valor del donado es de: ${valortele:,}')
+print(f'El valor del  donad  es de: ${valosiste:,}')
 print(f'El valor del  donad es de: ${valoradmi:,}')
 
 #java
