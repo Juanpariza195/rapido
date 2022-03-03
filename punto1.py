9b4f61ad810abe78e6f66b9941b7f6a4ac7911c2
juan ariza <jariza36@cuc.edu.co>
2022-03-02 20:51:37 -0500

    commit inicial

---
+++ punto1.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar  2 20:50:32 2022
+
+@author: jariza36
+"""
+
+donaciondevalor = int(input("valor total"))
+valortele = valordonado*0.1
+valosiste = valordonado*0.25
+valoradmi = valordonado*0.35
+vcontabilidad = valortele - valosiste - valoadmi
+
+print(f'El valor del donado es de: ${valortel:,}')
+print(f'El valor del  donad  es de: ${valosistee:,}')
+print(f'El valor del  donad es de: ${valoradmi:,}')
